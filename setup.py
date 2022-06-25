2050e2049e95d143512e6f90a385c3e5a053e539
srinu <chappidi@rhrk.uni-kl.de>
2022-06-25 22:45:08 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Analysis on COVID 19 Data from various countries',
+    author='Sreenivasulu Subaiya Chappidi',
+    license='',
+)
